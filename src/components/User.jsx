7d1a0f86e0eab9bfeaabd6d1f7e126f4adae5327
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-05-27 19:16:03 -0400

    Create function getAllUsers

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 function User(){
+
+    let[user,setUsers]=useState([]);
+
+    async function getAllUsers(){
+        let response=await fetch('https://dummyjson.com/users');
+        let result=await response.json();
+        setUsers(result.users);
+
+        console.log(user);
+
+    };
+
+    
+
+
     return(
+        
         <>
+
         </>
     );
 }
